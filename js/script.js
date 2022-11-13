c69b6f5f40210e8f56382846987c2eebd8829cd4
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2022-11-13 15:17:08 +0300

    for added, user rating

--- js/script.js
+++ js/script.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('One of the last movies you saw?', ''), 
-        b = prompt('How do you rate it?', ''),
-        c = prompt('One of the last movies you saw?', ''), 
-        d = prompt('How do you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of the last movies you saw?', ''), 
+            b = prompt('How do you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Watched few movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are a classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are a movie buff');
+} else {
+    console.log('Error!');
+}
 
 console.log(personalMovieDB);
